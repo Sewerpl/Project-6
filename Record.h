dc1419ce797b4274837f3e3c2896b8aebbe21e30
PC <seweryn.contact@gmail.com>
2025-01-20 15:37:50 +0100

    Konstruktor do inicjalizacji rekordu

--- Record.h
+++ Record.h
@@ -10,6 +10,17 @@ class Record {
 public:
     std::string date;  // Data w formacie YYYY-MM-DD
     std::string time;  // Godzina w formacie HH:MM
+    double autokonsumpcja;
+    double eksport;
+    double import_;
+    double pobor;
+    double produkcja;
+
+    // Konstruktor do inicjalizacji rekordu
+    Record(const std::string& date, const std::string& time, double autokonsumpcja, double eksport,
+           double import_, double pobor, double produkcja)
+        : date(date), time(time), autokonsumpcja(autokonsumpcja), eksport(eksport),
+          import_(import_), pobor(pobor), produkcja(produkcja) {}
 
 };
 
