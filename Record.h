d55b58ced38dfb6d865cf8ea1cba38b353a39978
PC <seweryn.contact@gmail.com>
2025-01-20 15:37:15 +0100

    Data w formacie YYYY-MM-DD

---
+++ Record.h
@@ -0,0 +1,15 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+// Klasa reprezentująca jeden punkt pomiarowy (rekord danych)
+class Record {
+public:
+    std::string date;  // Data w formacie YYYY-MM-DD
+
+};
+
+#endif // RECORD_H
