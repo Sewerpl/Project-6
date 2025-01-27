2dc6c2effffed679badb91cbd1ca6fff3fe2cde7
PC <seweryn.contact@gmail.com>
2025-01-27 11:58:27 +0100

    Tutaj będzie struktura hierarchiczna

---
+++ Tree.h
@@ -0,0 +1,14 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <map>
+#include <vector>
+#include "Record.h"
+
+class Tree {
+public:
+    // Tutaj będzie struktura hierarchiczna
+
+};
+
+#endif // TREE_H
