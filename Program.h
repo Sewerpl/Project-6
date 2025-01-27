eb3fa32d828c16f68e82b3841265aee49e65a595
PC <seweryn.contact@gmail.com>
2025-01-27 12:04:57 +0100

    Drzewo danych

---
+++ Program.h
@@ -0,0 +1,14 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include <string>
+#include <fstream>
+#include "Tree.h"
+
+class Program {
+private:
+    Tree tree; // Drzewo danych
+
+};
+
+#endif // PROGRAM_H
