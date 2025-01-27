28d55fcbdcd40b7e88e5bb8f32980eaf9c7f0a2a
PC <seweryn.contact@gmail.com>
2025-01-27 12:17:31 +0100

    Wszytywanie zakończone powodzeniem

---
+++ Program.cpp
@@ -0,0 +1,31 @@
+void Program::loadCSV(const std::string& fileName) {
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cerr << "Nie można otworzyć pliku: " << fileName << std::endl;
+        return;
+    }
+
+    std::ofstream log("log_data.txt");
+    std::ofstream errorLog("log_error_data.txt");
+
+    std::string line;
+    while (std::getline(file, line)) {
+        Record record;
+        if (parseLine(line, record)) {
+            tree.addRecord(record);
+            validRecords++;
+            logMessage("Poprawny rekord: " + line, "log_data.txt");
+        } else {
+            invalidRecords++;
+            logMessage("Błędny rekord: " + line, "log_error_data.txt");
+        }
+    }
+
+    file.close();
+    log.close();
+    errorLog.close();
+
+    std::cout << "Wczytywanie zakończone. Poprawne: " << validRecords << ", Błędne: " << invalidRecords << std::endl;
+}
+
+}
