db209c1802d137567c15b6a46f306470fa9856ee
PC <seweryn.contact@gmail.com>
2025-01-27 12:09:27 +0100

    int Tree GetQuater

--- Tree.cpp
+++ Tree.cpp
@@ -6,3 +6,12 @@ void Tree::addRecord(const Record& record) {
     years[year].months[month].days[day].quarters[quarter].records.push_back(record);
 }
 
+int Tree::getQuarter(const std::string& time) {
+    int hour, minute;
+    sscanf(time.c_str(), "%d:%d", &hour, &minute);
+
+    if (hour < 6) return 0;
+    else if (hour < 12) return 1;
+    else if (hour < 18) return 2;
+    else return 3;
+}
